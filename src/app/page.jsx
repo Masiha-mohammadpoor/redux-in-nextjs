b7fd73f671c0c777abddb0afab11b09aeea2e895
masiham <masihmooohmmadpoor@gmail.com>
2024-06-23 12:56:39 +0330

    created store

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,16 @@
+"use client"
 import Signin from "@/components/Signin";
-
+import { Provider } from "react-redux";
+import { store } from "@/redux/store";
 
 const Home = () => {
   return (
-    <main>
-      <Signin/>
-    </main>
+    <Provider store={store}>
+      <main>
+        <Signin />
+      </main>
+    </Provider>
   );
-}
- 
+};
+
 export default Home;
